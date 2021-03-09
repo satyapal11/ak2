8299fc8c550687ee6a5e14b86d00f4cae09c0069
satyapal11 <80347292+satyapal11@users.noreply.github.com>
2021-03-09 21:51:04 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,88 @@
+function setImg1(){
+document.getElementById("card1").src="image1/priya.mp4";
+
+}
+function backImg1(){
+document.getElementById("card1").src="image1/baniya.mp4";
+}
+function setImg2(){
+document.getElementById("card2").src="image1/swarna.mp4";
+}
+function backImg2(){
+	document.getElementById("card2").src="image1/priya.mp4";
+}
+function setImg3(){
+document.getElementById("card3").src="image1/baniya.mp4";
+}
+function backImg3(){
+	document.getElementById("card3").src="image1/sona.mp4";
+}
+function setImg4(){
+document.getElementById("card4").src="image1/sona.mp4";
+}
+function backImg4(){
+	document.getElementById("card4").src="image1/swarna.mp4";
+}
+//image magic
+function inMagic1()
+{
+document.getElementById("magicBox1").src="image1/a1.jpg";
+}
+function inMagic2()
+{
+document.getElementById("magicBox2").src="image1/giphy1.webp";
+
+}
+function halva(){
+	document.getElementById("corona").style="display:true";
+}
+// function inMagic3()
+// {
+// document.getElementById("magicBox3").src="image1/a3.jpg";
+// }
+// function inMagic4()
+// {
+// document.getElementById("magicBox4").src="image1/a4.jpg";
+// }
+// function inMagic5()
+// {
+// document.getElementById("magicBox5").src="image1/a5.jpg";
+// }
+// function inMagic6()
+// {
+// document.getElementById("magicBox6").src="image1/albumImg1.jpg";
+// }
+// function inMagic7()
+// {
+// document.getElementById("magicBox7").src="image1/albumImg1.jpg";
+// }
+
+
+// function outMagic1()
+// {
+// document.getElementById("magicBox1").src="image1/backimage.jpg";
+// }
+// function outMagic2()
+// {
+// document.getElementById("magicBox2").src="image1/backimage.jpg";
+// }
+// function outMagic3()
+// {
+// document.getElementById("magicBox3").src="image1/backimage.jpg";
+// }
+// function outMagic4()
+// {
+// document.getElementById("magicBox4").src="image1/backimage.jpg";
+// }
+// function outMagic5()
+// {
+// document.getElementById("magicBox5").src="image1/backimage.jpg";
+// }
+// function outMagic6()
+// {
+// document.getElementById("magicBox6").src="image1/backimage.jpg";
+// }
+// function outMagic7()
+// {
+// document.getElementById("magicBox7").src="image1/backimage.jpg";
+// }
